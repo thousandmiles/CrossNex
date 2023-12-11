4c4f8016c123f2fd12b1ae16208a3f0a9cdd749e
thousandmiles <longxy98@foxmail.com>
2023-12-11 22:45:24 +0800

    main function modified

--- CrossNex/main.cpp
+++ CrossNex/main.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include "dataservice.h"
 
 #include <QApplication>
 
@@ -6,6 +7,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+    //w.show();
+    DataService *data = new DataService();
+    data->StartService();
+
     return a.exec();
 }
