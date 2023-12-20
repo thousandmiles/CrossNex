1947c3d22e28e1663af5e30248029321082783db
thousandmiles <longxy98@foxmail.com>
2023-12-21 00:20:07 +0800

    add name check: rename and create new node

--- CrossNex/navigationtree.cpp
+++ CrossNex/navigationtree.cpp
@@ -3,6 +3,8 @@
 #include "newinstancedialog.h"
 #include <QInputDialog>
 #include <QMessageBox>
+#include "customtreewidgetitem.h"
+#include <QTreeWidgetItemIterator>
 
 NavigationTree::NavigationTree(QWidget *parent):
     QTreeWidget(parent),
@@ -18,11 +20,16 @@ NavigationTree::NavigationTree(QWidget *parent):
     setContextMenuPolicy(Qt::CustomContextMenu);
 
     this->setHeaderLabel("实例列表");
+    QFont font = this->headerItem()->font(0);
+    font.setBold(true);
+    this->headerItem()->setFont(0, font);
+
 
     rootNode->setText(0, "新建");
     rootNode->setExpanded(true);  // 设置根节点默认展开
-
     rootNode->setIcon(0, QIcon(new_root));
+
+    isParentFolderNameUsed = false;
 
     connect(this, &NavigationTree::customContextMenuRequested, this, &NavigationTree::showContextMenu);
 }
@@ -60,10 +67,72 @@ void NavigationTree::traverseTree(QTreeWidgetItem *item) {
 
 }
 
-void NavigationTree::clearTreeSet()
+/* 获取当前节点下的所有子节点，包含文件夹和实例 */
+void NavigationTree::setTreeSet(QTreeWidgetItem *item)
 {
     folderSet.clear();
     instanceSet.clear();
+    traverseTree(item);
+}
+
+/* 获取当前节点的所有同级子节点，不搜索同级节点的子节点 */
+void NavigationTree::setCurrentLevelSet(QTreeWidgetItem *item)
+{
+
+    if (!item->parent())
+    {
+        return;
+    }
+
+    currentLevelInstanceSet.clear();
+    currentLevelFolderSet.clear();
+
+    QTreeWidgetItem* parentNode = item->parent();
+    QTreeWidgetItemIterator it(item->parent());
+    while ((*it))
+    {
+        if ((*it)->parent() && (*it)->parent() == parentNode)
+        {
+            QString currentName = (*it)->text(0);
+
+            if((*it)->type() == CustomTreeWidgetItem::NodeType)
+            {
+                currentLevelInstanceSet<<currentName;
+            }
+            else
+            {
+                currentLevelFolderSet<<currentName;
+            }
+        }
+        ++it;
+    }
+}
+
+/* 获取当前节点的下一级所有的同级子节点，不搜索同级节点的子节点 */
+void NavigationTree::setNextLevelSet(QTreeWidgetItem *item)
+{
+    nextLevelInstanceSet.clear();
+    nextLevelFolderSet.clear();
+
+    QTreeWidgetItem* currNode = item;
+    QTreeWidgetItemIterator it(item);
+    while ((*it))
+    {
+        if ((*it)->parent() && (*it)->parent() == currNode)
+        {
+            QString currentName = (*it)->text(0);
+
+            if((*it)->type() == CustomTreeWidgetItem::NodeType)
+            {
+                nextLevelInstanceSet<<currentName;
+            }
+            else
+            {
+                nextLevelFolderSet<<currentName;
+            }
+        }
+        ++it;
+    }
 }
 
 bool NavigationTree::isDeleteFolder()
@@ -73,6 +142,29 @@ bool NavigationTree::isDeleteFolder()
     msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
 
     return msgBox.exec() == QMessageBox::Yes ? true : false;
+}
+
+bool NavigationTree::isDeleteInstance()
+{
+    QMessageBox msgBox;
+    msgBox.setText("是否要删除当前实例？");
+    msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+
+    return msgBox.exec() == QMessageBox::Yes ? true : false;
+}
+
+void NavigationTree::mousePressEvent(QMouseEvent *event) {
+    if (event->button() == Qt::LeftButton) {
+        QTreeWidgetItem *item = itemAt(event->pos());
+        currentNode = item;
+        if (item)
+        {
+            // 打开图表区
+        }
+    }
+
+    // 调用基类的事件处理函数
+    QTreeWidget::mousePressEvent(event);
 }
 
 void NavigationTree::showContextMenu(const QPoint &pos)
@@ -89,8 +181,9 @@ void NavigationTree::showContextMenu(const QPoint &pos)
     QAction *deleteFolderAction = nullptr;
     QAction *renameNodeAction = nullptr;
 
-    clearTreeSet();
-    traverseTree(currentNode);
+    setTreeSet(currentNode);
+    setCurrentLevelSet(currentNode);
+    setNextLevelSet(currentNode);
 
     if (currentNode == rootNode.data())
     {
@@ -99,9 +192,7 @@ void NavigationTree::showContextMenu(const QPoint &pos)
 
         connect(addFolderAction, &QAction::triggered, this, &NavigationTree::createFolder);
         connect(addAction, &QAction::triggered, this, &NavigationTree::createInstance);
-
-        QPoint pos = QCursor::pos();
-        contextMenu.exec(pos);
+        contextMenu.exec(mapToGlobal(pos));
     }
     else
     {
@@ -130,11 +221,15 @@ void NavigationTree::showContextMenu(const QPoint &pos)
         }
         else if (deleteAction && selectedAction == deleteAction)
         {
-            deleteCurrentNode();
+            bool del = isDeleteInstance();
+            if (del)
+            {
+                deleteCurrentNode();
+            }
         }
         else if (renamFolderAction && selectedAction == renamFolderAction)
         {
-
+            renameNode();
         }
         else if (addFolderAction && selectedAction == addFolderAction)
         {
@@ -142,7 +237,7 @@ void NavigationTree::showContextMenu(const QPoint &pos)
         }
         else if (renameNodeAction && selectedAction == renameNodeAction)
         {
-
+            renameNode();
         }
         else if (deleteFolderAction && selectedAction == deleteFolderAction)
         {
@@ -160,17 +255,19 @@ void NavigationTree::createFolder()
 
     bool ok = false;
     QString folderName;
+
     while (!ok) {
         folderName = QInputDialog::getText(nullptr, "输入", "请输入文件夹名:", QLineEdit::Normal, "", &ok);
-        if (!ok || folderName == currentNode->text(0))
+        if (!ok)
         {
             break;
         }
 
-        if (folderSet.contains(folderName))
+        if (nextLevelFolderSet.contains(folderName))
         {
             QMessageBox::warning(nullptr, "Error", "文件夹已存在，请重新输入。");
             ok = false;  // 设置标志为false，继续循环
+            continue;
         }
     }
 
@@ -182,7 +279,8 @@ void NavigationTree::createFolder()
 void NavigationTree::createInstance()
 {
     NewInstanceDialog newInstanceDlg;
-    newInstanceDlg.setInstanceSet(instanceSet);
+
+    newInstanceDlg.setInstanceSet(nextLevelInstanceSet);
 
     if (newInstanceDlg.exec() == QDialog::Accepted)
     {
@@ -215,6 +313,48 @@ void NavigationTree::deleteCurrentNode()
         int relativeIndex = parentItem->indexOfChild(currentNode);
         parentItem->takeChild(relativeIndex);
         delete currentNode;
+    }
+}
+
+void NavigationTree::renameNode()
+{
+    bool ok = false;
+    QString newName;
+
+    setCurrentLevelSet(currentNode);
+
+    while (!ok) {
+        newName = QInputDialog::getText(nullptr, "重命名", "请输入新的名字:", QLineEdit::Normal, "", &ok);
+        if (!ok)
+        {
+            break;
+        }
+
+        if (newName == currentNode->text(0))
+        {
+            QMessageBox::warning(nullptr, "Error", "名字已存在，请重新输入。");
+            ok = false;  // 设置标志为false，继续循环
+            continue;
+        }
+
+        if (currentNode->type() == CustomTreeWidgetItem::NodeType && currentLevelInstanceSet.contains(newName))
+        {
+            QMessageBox::warning(nullptr, "Error", "名字已存在，请重新输入。");
+            ok = false;  // 设置标志为false，继续循环
+            continue;
+        }
+
+        if (currentNode->type() == CustomTreeWidgetItem::FolderType && currentLevelFolderSet.contains(newName))
+        {
+            QMessageBox::warning(nullptr, "Error", "名字已存在，请重新输入。");
+            ok = false;  // 设置标志为false，继续循环
+            continue;
+        }
+
+    }
+
+    if (ok && !newName.isEmpty()) {
+        currentNode->setText(0, newName);
     }
 }
 
--- CrossNex/navigationtree.h
+++ CrossNex/navigationtree.h
@@ -5,8 +5,7 @@
 #include <QTreeWidget>
 #include <QWidget>
 #include <QMenu>
-
-#include "customtreewidgetitem.h"
+#include <QMouseEvent>
 
 constexpr const char* delete_folder = ":/icon/plus/resource/delete_folder.png";
 constexpr const char* delete_instance = ":/icon/plus/resource/delete_instance.png";
@@ -17,6 +16,8 @@ constexpr const char* rename_folder = ":/icon/plus/resource/rename.png";
 constexpr const char* rename_instance = ":/icon/plus/resource/rename.png";
 constexpr const char* folder = ":/icon/plus/resource/folder.png";
 constexpr const char* instance = ":/icon/plus/resource/instance.png";
+constexpr const char* expanded = ":/icon/plus/resource/expanded_triangle.png";
+constexpr const char* collapsed = ":/icon/plus/resource/collapsed_triangle.png";
 
 class NavigationTree : public QTreeWidget
 {
@@ -26,8 +27,14 @@ public:
     void dropEvent(QDropEvent *event) override;
 
     void traverseTree(QTreeWidgetItem *item);
-    void clearTreeSet();
+    void setTreeSet(QTreeWidgetItem *item);
+    void setCurrentLevelSet(QTreeWidgetItem *item);
+    void setNextLevelSet(QTreeWidgetItem *item);
     bool isDeleteFolder();
+    bool isDeleteInstance();
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
 
 private slots:
     void showContextMenu(const QPoint &pos);
@@ -40,11 +47,18 @@ private:
     QMenu contextMenu;
     QSet<QString> instanceSet;
     QSet<QString> folderSet;
+    QSet<QString> currentLevelInstanceSet;
+    QSet<QString> currentLevelFolderSet;
+    QSet<QString> nextLevelInstanceSet;
+    QSet<QString> nextLevelFolderSet;
+
+    bool isParentFolderNameUsed;
 
 private:
     void addFolder(const QString &folderName);
     void addInstance(const QString &instanceName);
     void deleteCurrentNode();
+    void renameNode();
 
 };
 
