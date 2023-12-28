6350434832f82862d8be0be311a9dc05055c2743
thousandmiles <longxy98@foxmail.com>
2023-12-28 08:17:41 +0800

    remove useless functions and classes

--- CrossNex/echartwebengineview.cpp
+++
@@ -1,9 +0,0 @@
-#include "echartwebengineview.h"
-#include <QFile>
-
-EchartWebEngineView::EchartWebEngineView(QWidget *parent):
-    QWebEngineView(parent)
-{
-    this->load(QUrl("qrc:/echart/EChart/line-simple.html"));
-
-}
