bdf7e5648c003d6c417faa6fc3ee948db4ebe9f9
thousandmiles <longxy98@foxmail.com>
2024-01-01 15:07:52 +0800

    add hello page and add methods to get data

--- CrossNex/tab.cpp
+++ CrossNex/tab.cpp
@@ -1,11 +1,71 @@
 #include "tab.h"
 #include <QVBoxLayout>
+#include <QJsonDocument>
 
-Tab::Tab(const QString& url, QWidget *parent)
-    : QWidget{parent}
+Tab::Tab(const QString& ip, QWidget *parent)
+    : QWidget{parent},
+    ip(ip)
+{
+    dataService = new DataService(this);
+
+    webEngineView = new QWebEngineView(this);
+    webEngineView->load(QUrl("qrc:/echart/EChart/main-page.html"));
+
+    webChannel = new QWebChannel(webEngineView);
+    webTransport = new WebTransport(webEngineView->page());
+
+    webChannel->registerObject(QStringLiteral("webTransport"), webTransport);
+    webEngineView->page()->setWebChannel(webChannel);
+
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    layout->addWidget(webEngineView);
+    setLayout(layout);
+
+    connect(&timer, &QTimer::timeout, this, &Tab::fetchData);
+
+    connect(dataService, &DataService::control_CPU_Info_DataReady, this, &Tab::updateCPUData);
+    connect(dataService, &DataService::control_CPU_Time_DataReady, this, &Tab::updateCPUTime);
+    connect(dataService, &DataService::control_Process_Time_pid_DataReady, this, &Tab::updateProcessTimePid);
+    connect(dataService, &DataService::control_Disk_List_DataReady, this, &Tab::updateDiskList);
+    connect(dataService, &DataService::control_Process_Memory_pid_DataReady, this, &Tab::updateProcessMemoryPid);
+    connect(dataService, &DataService::control_Machine_Memory_DataReady, this, &Tab::updateMachineMemory);
+    connect(dataService, &DataService::control_Process_Info_DataReady, this, &Tab::updateProcessInfo);
+
+    connect(dataService, &DataService::errorOccurred, this, &Tab::handleError);
+
+    // 获取URL数据
+    dataService->setIP(ip);
+
+    timer.start(1000);
+}
+
+
+void Tab::updateCPUData(const QByteArray &data)
+{
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(data);
+    QJsonObject cpuJson = jsonDoc.object();
+
+    qDebug()<<"cpuJson: "<< cpuJson;
+    webTransport->sendCPUDataToJavaScript(cpuJson);
+}
+
+
+void Tab::handleError(const QString &error)
+{
+    qDebug()<<"Error";
+}
+
+
+void Tab::fetchData() {
+
+    dataService->FetchData(ID_CPU_Info);
+}
+
+
+Hello::Hello(QWidget *parent): QWidget{parent}
 {
     webEngineView = new QWebEngineView(this);
-    webEngineView->load(QUrl(url));
+    webEngineView->load(QUrl("qrc:/hello/EChart/hello.html"));
 
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->addWidget(webEngineView);
--- CrossNex/tab.h
+++ CrossNex/tab.h
@@ -3,17 +3,85 @@
 
 #include <QWidget>
 #include <QWebEngineView>
+#include <QWebChannel>
+#include <QMessageBox>
+#include <QJsonObject>
+#include <QTimer>
+#include "dataservice.h"
+
+
+class WebTransport : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QJsonObject CPUJsonData MEMBER CPUJsonData NOTIFY dataChanged)
+
+public:
+    explicit WebTransport (QWebEnginePage *page, QObject *parent = nullptr)
+        : QObject(parent), webPage(page) {}
+
+public slots:
+    void sendCPUDataToJavaScript(const QJsonObject &data) {
+        // 将数据从Qt传递到JavaScript
+        setProperty("CPUJsonData", data);
+    }
+signals:
+    void dataChanged(const QJsonObject &CPUJsonData);
+
+private:
+    QWebEnginePage *webPage;
+    QJsonObject CPUJsonData;
+};
+
+
+
+//-------------------------
+//  Hello Page Tab class
+//-------------------------
+class Hello: public QWidget
+{
+    Q_OBJECT
+
+public:
+    Hello(QWidget *parent = nullptr);
+private:
+    QWebEngineView *webEngineView;
+};
+
+
+
+//-------------------------
+//  Normal Tab class
+//-------------------------
 
 class Tab : public QWidget
 {
     Q_OBJECT
 public:
-    explicit Tab(const QString& url, QWidget *parent = nullptr);
+    explicit Tab(const QString& ip, QWidget *parent = nullptr);
+
+public slots:
+    void fetchData();          // test function
+
+private slots:
+    void updateCPUData(const QByteArray &data);
+    void updateCPUTime(const QByteArray &data);
+    void updateProcessTimePid(const QByteArray &data);
+    void updateDiskList(const QByteArray &data);
+    void updateProcessMemoryPid(const QByteArray &data);
+    void updateMachineMemory(const QByteArray &data);
+    void updateProcessInfo(const QByteArray &data);
+
+    void handleError(const QString &error);
 
 signals:
 
 private:
     QWebEngineView *webEngineView;
+    QWebChannel *webChannel;
+    WebTransport* webTransport;
+    QString ip;
+    QTimer timer;
+    DataService *dataService;
 };
 
 #endif // TAB_H
